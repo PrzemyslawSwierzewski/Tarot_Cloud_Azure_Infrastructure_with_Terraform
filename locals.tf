96a176fd841eb95020331d197881ce468d4249c3
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2025-08-25 00:37:17 +0200

    Remove unnecessary local values

--- locals.tf
+++ locals.tf
@@ -1,14 +1,4 @@
 locals {
   resource_group_name = "Tarot-cloud"
   rg_location         = "East US"
-  vnets = {
-    vnet1 = {
-      address_space = "10.0.0.0/16",
-      subnet_prefix = "10.0.1.0/24"
-    }
-    vnet2 = {
-      address_space = "10.1.0.0/16",
-      subnet_prefix = "10.1.1.0/24"
-    }
-  }
 }

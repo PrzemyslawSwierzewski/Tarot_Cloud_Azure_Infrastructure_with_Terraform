3203a5f7fea7287bd7d1da03d966bbd4a2eef0d9
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2025-08-25 09:09:17 +0200

    Add NSGs and expose NIC from networking module outputs

--- modules/networking/locals.tf
+++ modules/networking/locals.tf
@@ -15,5 +15,56 @@ locals {
   tarot_cloud_network_name = "tarot-cloud-network"
   tarot_cloud_nic_name = "tarot-cloud-nic"
   public_ip_name = "tarot-cloud-public-ip"
+  network_security_group_name = "tarot-cloud-nsg"
+  security_rules = {
+    Inbound = {
+      Allow80port = {
+        name                       = "Allow80port"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+      },
+      Allow443port = {
+        name                       = "Allow443port"
+        priority                   = 101
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+      }
+      Allow22port = {
+        name                       = "Allow22port"
+        priority                   = 102
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+      }
+    }
+    Outbound = {
+      AllowAll = {
+        name                       = "AllowAll"
+        priority                   = 100
+        direction                  = "Outbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+      }
+    }
+  }
 }
 
--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,3 +1,7 @@
+output "tarot_cloud_nic" {
+  value = { for k, nic in azurerm_network_interface.tarot_cloud_nic : k => nic.id }
+}
+
 output "tarot_cloud_public_ip" {
-  value = { for k, PIP in azurerm_public_ip.tarot_cloud_public_ip : k => PIP.ip_address }
+  value = { for k, ip in azurerm_public_ip.tarot_cloud_public_ip : k => ip.ip_address }
 }

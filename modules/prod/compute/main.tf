f9d9dac19273ae4ec463508e42accd2d9e3cf647
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2025-08-27 13:10:12 +0200

    configure Terraform to always use the latest compatible extension version for azure monitor

--- modules/prod/compute/main.tf
+++ modules/prod/compute/main.tf
@@ -39,6 +39,6 @@ resource "azurerm_virtual_machine_extension" "ama_linux" {
   virtual_machine_id         = azurerm_linux_virtual_machine.tarot_cloud_linux_VM.id
   publisher                  = "Microsoft.Azure.Monitor"
   type                       = local.AzureMonitorLinuxAgent
-  type_handler_version       = "1.36.1"
+  type_handler_version       = "*"
   auto_upgrade_minor_version = true
 }
